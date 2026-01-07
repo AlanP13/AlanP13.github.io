4fee881be13ce493b48ecfe02ce2dfde81dc8eff
Alan Palayil <alan.palayil@gmail.com>
2026-01-06 20:16:54 -0500

    Updated About Me with final style and place holder images. Also formatted all documents to match the correct flow.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   // base: "/"  // optional; default is fine for user sites
   site: "https://alanp13.github.io",
 
-  integrations: [mdx()]
+  integrations: [mdx(), react()]
 });
